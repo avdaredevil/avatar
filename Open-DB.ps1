152f915f0604ae51c2b6bc40e7c192b681eb7322
Apoorv Verma [AP] <avdaredevil@gmail.com>
2016-05-07 13:44:58 -0400

    Removed Scripts related to my work

--- Open-DB.ps1
+++
@@ -1,4 +0,0 @@
-param([Switch]$AsWorker)
-$Cred = "AP","AProcks1#","admin"
-Write-Host -nonewline "Logging in as: ";Write-Host -f 2 $Cred[0]
-mongo -u $Cred[0] -p $Cred[1] --authenticationDatabase $Cred[2] JMeter-Test
